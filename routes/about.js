abc49e957f99ca225e17751e7c3ed1eb32e96e41
kdh6194 <kdh6194@naver.com>
2023-02-16 12:47:54 +0900

    express view engine - handlebars

--- routes/about.js
+++ routes/about.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const path = require('path');
 const router = express.Router();
-const html = 'text/html; charset=utf-8';
+
 // show about page
 router.get('/',(req, res)=>{
-        res.sendFile(path.join(__dirname,'../public','about.html'));
+        // res.sendFile(path.join(__dirname,'../public','about.html'));
+        res.render('about',{title:'about'});
+        // option 부분에 해당하는 정보는 따로 지정해야 적용되는듯
 });
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,13 @@ const router = express.Router();
 const html = 'text/html; charset=utf-8';
 // show index page
 router.get('/',(req, res)=>{
-res.sendFile(path.join(__dirname,'../public','index.html'));
-}); //현재 있는 곳이 라우터라서 상위폴더에 접근한것
+// res.sendFile(path.join(__dirname,'../public','index.html')); 이렇게 적지말고
+    // handledbars 뷰 엔진으로 응답처리
+
+    res.render('index',{title:'index'});
+});
+//현재 있는 곳이 라우터라서 상위폴더에 접근한것
+
 // 단순한 그림파일
 // 일일히 라우팅을 넣는게 아주 귀찮음
 // router.get('/rubber-duck.png',(req, res)=>{
